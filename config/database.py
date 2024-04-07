01abd0f571ac2fbbb882dd1564252fc399c56b0a
Raul Albuquerque <130876489+Raul-Albuquerque@users.noreply.github.com>
2024-04-07 15:16:18 -0300

    Update database.py

--- config/database.py
+++ config/database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 
 
-client = MongoClient('mongodb+srv://raulmalbuquerque2015:admin1234@portfolio-api.0fqot3o.mongodb.net/?retryWrites=true&w=majority&appName=portfolio-api')
+client = MongoClient('mongodb+srv://client:client1234@portfolio-api.0fqot3o.mongodb.net/?retryWrites=true&w=majority&appName=portfolio-api')
 
 db = client.portfolio_db
 
