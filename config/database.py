15f3b0edec0c2dd5d013b6526a13d96ba9051538
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-04-07 12:45:20 -0300

    chore: update db collections

--- config/database.py
+++ config/database.py
@@ -1,7 +1,22 @@
 from pymongo import MongoClient
 
+
+
 client = MongoClient('mongodb+srv://raulmalbuquerque2015:admin1234@portfolio-api.0fqot3o.mongodb.net/?retryWrites=true&w=majority&appName=portfolio-api')
 
 db = client.portfolio_db
 
-collection_name = db ['portfolio_collection']
+
+# DEVELOPERS COLLECTION
+
+developers_collection = db ['developers']
+
+
+# PROJECTS COLLECTION
+
+projects_collection = db ['projects']
+
+
+# EXPERIENCES COLLECTION
+
+experiences_collection = db ['experiences']
