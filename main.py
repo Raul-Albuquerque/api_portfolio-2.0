e91a7f848611798d6f32a00942c32f0e6f05f1e8
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-05-06 22:02:33 -0300

    fix: Cors bug

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from routes.dev_router import dev_router
 from routes.project_router import project_router
 from routes.freelances_router import freelances_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],  
+)
 
 # DEV ROUTER
 
